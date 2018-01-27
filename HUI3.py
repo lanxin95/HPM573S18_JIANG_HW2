fc5b43c38ab1d6408cee4cead8fa1fa3d3a0455d
lanxin95 <lexyjiang@gmail.com>
2018-01-27 17:07:23 -0500

    only multi

--- HUI3.py
+++ HUI3.py
@@ -3,14 +3,6 @@
 # The user will specify the level of 8 attributes in HUI3 instrument and gets the corresponding health score.
 C1 = 1.371
 C2 = 0.371
-SingCoefficients = {'Vision':	    [1,	0.95, 0.73,	0.59, 0.38, 0],
-                    'Hearing':	    [1,	0.86, 0.71,	0.48, 0.32,	0],
-                    'Speech':	    [1,	0.82, 0.67,	0.41, 0],
-                    'Ambulation':   [1, 0.83, 0.67, 0.36, 0.16, 0],
-                    'Dexterity':    [1, 0.88, 0.73, 0.45, 0.2, 0],
-                    'Emotion':	    [1,	0.91, 0.73, 0.33, 0],
-                    'Cognition':    [1, 0.86, 0.92, 0.7,  0.32, 0],
-                    'Pain':	        [1,	0.92, 0.77,	0.48, 0]}
 
 MultCoefficients = {'Vision':	    [1, 0.98, 0.89, 0.84, 0.75,	0.61],
                     'Hearing':	    [1, 0.95, 0.89, 0.80, 0.74,	0.61],
@@ -53,40 +45,18 @@ def get_score(Vision, Hearing, Speech, Ambulation, Dexterity, Emotion, Cognition
     if not (Pain in [1, 2, 3, 4, 5]):
         raise ValueError('Pain level can take only 1, 2, 3, 4, 5')
 
-    count = 0
-    for num in [Vision, Hearing, Speech, Ambulation, Dexterity, Emotion, Cognition, Pain]:
-        if num == 1:
-            count += 1
-
-
-    if  count >= 7:
-        print("HUI3 Single-Attribute Utility Function")
-        score=1
-        score *= SingCoefficients['Vision'][Vision - 1]
-        score *= SingCoefficients['Hearing'][Hearing - 1]
-        score *= SingCoefficients['Speech'][Speech - 1]
-        score *= SingCoefficients['Ambulation'][Ambulation - 1]
-        score *= SingCoefficients['Dexterity'][Dexterity - 1]
-        score *= SingCoefficients['Emotion'][Emotion - 1]
-        score *= SingCoefficients['Cognition'][Cognition - 1]
-        score *= SingCoefficients['Pain'][Pain - 1]
-    else:
-        print("HUI3 Multi-Attribute Utility Function")
-        score = C1
-        score *= MultCoefficients['Vision'][Vision - 1]
-        score *= MultCoefficients['Hearing'][Hearing - 1]
-        score *= MultCoefficients['Speech'][Speech - 1]
-        score *= MultCoefficients['Ambulation'][Ambulation - 1]
-        score *= MultCoefficients['Dexterity'][Dexterity - 1]
-        score *= MultCoefficients['Emotion'][Emotion - 1]
-        score *= MultCoefficients['Cognition'][Cognition - 1]
-        score *= MultCoefficients['Pain'][Pain - 1]
-        score -= C2
+    print("HUI3 Multi-Attribute Utility Function")
+    score = C1
+    score *= MultCoefficients['Vision'][Vision - 1]
+    score *= MultCoefficients['Hearing'][Hearing - 1]
+    score *= MultCoefficients['Speech'][Speech - 1]
+    score *= MultCoefficients['Ambulation'][Ambulation - 1]
+    score *= MultCoefficients['Dexterity'][Dexterity - 1]
+    score *= MultCoefficients['Emotion'][Emotion - 1]
+    score *= MultCoefficients['Cognition'][Cognition - 1]
+    score *= MultCoefficients['Pain'][Pain - 1]
+    score -= C2
     return score
 
 
-print("Score for ", [2, 1, 1, 2, 1, 2, 1, 3])
-print(get_score(2, 1, 1, 2, 1, 2, 1, 3))
 
-print("Score for ", [1, 1, 1, 1, 1, 2, 1, 1])
-print(get_score(1, 1, 1, 1, 1, 2, 1, 1))
---
+++ useHUI3.py
@@ -0,0 +1,9 @@
+import HUI3 as HUI
+
+print("Score for", 2, 1, 1, 2, 1, 2, 1, 3)
+print(HUI.get_score(2, 1, 1, 2, 1, 2, 1, 3))
+
+print("Score for", 1, 2, 3, 1, 1, 1, 1, 1)
+print(HUI.get_score(1, 2, 3, 1, 1, 1, 1, 1))
+
+
